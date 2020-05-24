1ac466430c81c421a22f8d939e58c25b00fe6600
Travis Goodspeed <travis@radiantmachines.com>
2020-05-24 12:23:44 -0400

    Initial commit, not much more than Hello World.

---
+++ codeplugtool/com/kk4vcz/codeplug/Main.java
@@ -0,0 +1,17 @@
+package com.kk4vcz.codeplug;
+
+/* Hey y'all,
+ * 
+ * This is a main class that can be run from the commandline as a
+ * standalone .jar file.
+ * 
+ * --KK4VCZ
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Codeplug Tool by KK4VCZ and Friends");
+	}
+
+}
